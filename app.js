4893b6381d4a585c0aa83f834daaa20880ab3839
Slenzy2 <tobiikupolati33@gmail.com>
2022-06-25 16:40:50 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const myName = document.getElementById("name");
+const height = document.getElementById("height");
+const country = document.getElementById("country");
+
+myName.innerHTML = "Ikupolati Tobi Emmanuel";
+height.innerHTML = 1.86 + "m";
+country.innerHTML = "Nigeria";
+
+
